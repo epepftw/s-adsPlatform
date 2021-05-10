b3367ada9127bc9d6bae8d81dc04d5a9b1b912fb
epepftw <ipinggxlirangg@gmail.com>
2021-05-11 01:27:09 +0800

    Pages added and upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FileUploadComponent } from './components/file-upload/file-upload.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { UploadComponent } from './pages/upload/upload.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MessagesComponent } from './pages/messages/messages.component';
+import { PlaylistComponent } from './pages/playlist/playlist.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FileUploadComponent
+    FileUploadComponent,
+    NavbarComponent,
+    UploadComponent,
+    DashboardComponent,
+    MessagesComponent,
+    PlaylistComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
